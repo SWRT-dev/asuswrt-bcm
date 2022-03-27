b642e994fcf67569669b2e664bfa1381fd3981d0
ZhaoWei Xu <paldier@hotmail.com>
2022-03-27 14:27:10 +0800

    Merge 386.46065 for RT-AX56U

--- release/src/router/www/require/menuTrees/menuTree_TUF.js
+++ release/src/router/www/require/menuTrees/menuTree_TUF.js
@@ -1,4 +1,4 @@
-﻿/* menuTree_TUF.js */
+/* menuTree_TUF.js */
 define(function(){
 	var menuTree = {
 		list: [
@@ -85,10 +85,10 @@ define(function(){
 				] 
 			},
 			{
-				menuName: "网易UU加速器",
+				menuName: "<#UU_Accelerator#>",
 				index: "menu_UU",
 				tab: [
-						{url: "UUAccelerator.asp", tabName: "网易UU加速器"},
+						{url: "UUAccelerator.asp", tabName: "<#UU_Accelerator#>"},
 						{url: "NULL", tabName: "__INHERIT__"}
 				]
 			},
@@ -101,10 +101,10 @@ define(function(){
 				]
 			},
 			{
-				menuName: "腾讯网游加速器",
+				menuName: "<#TencentAcceleration#>",
 				index: "menu_TencentAcceleration",
 				tab: [
-					{url: "GameBoost_Tencent.asp", tabName: "Tencent Game Acceleration"},
+					{url: "GameBoost_Tencent.asp", tabName: "<#TencentAcceleration#>"},
 					{url: "NULL", tabName: "__INHERIT__"}
 				]
 			},
@@ -173,7 +173,15 @@ define(function(){
 					{url: "NULL", tabName: "__INHERIT__"}
 				] 
 			},
-
+			{
+				menuName: "<#Softcenter_tool#>",
+				index: "menu_Tools",
+				tab: [
+					{url: "Tools_Sysinfo.asp", tabName: "Sysinfo"},
+					{url: "Softcenter.asp", tabName: "<#Softcenter_tool#>"},
+					{url: "NULL", tabName: "__INHERIT__"}
+				] 
+			},
 			/* ============================================================================================================ */
 
 			{
@@ -210,6 +218,7 @@ define(function(){
 					{url: "Advanced_GWStaticRoute_Content.asp", tabName: "<#menu5_2_3#>"},
 					{url: "Advanced_IPTV_Content.asp", tabName: "IPTV"},
 					{url: "Advanced_SwitchCtrl_Content.asp", tabName: "<#Switch_itemname#>"},
+					{url: "Advanced_SmartDNS_Content.asp", tabName: "SmartDNS"},
 					{url: "NULL", tabName: "__INHERIT__"}
 				] 
 			},
@@ -317,6 +326,39 @@ define(function(){
 					{url: "Main_Netstat_Content.asp", tabName: "Netstat"},
 					{url: "Main_WOL_Content.asp", tabName: "<#NetworkTools_WOL#>"},
 					{url: "Advanced_Smart_Connect.asp", tabName: "<#smart_connect_rule#>"},
+					{url: "NULL", tabName: "__INHERIT__"}
+				]
+			},
+			{
+				menuName: "Entware",
+				index: "menu_Split",
+				tab: [
+					{url: "NULL", tabName: "__HIDE__"}
+				]
+			},
+			{
+				menuName: "Entware",
+				index: "menu_Entware",
+				tab: [
+					{url: "Entware_start.asp", tabName: "<#Entware_home#>"},
+					{url: "Entware_installed.asp", tabName: "<#Entware_installed#>"},
+					{url: "Entware_list.asp", tabName: "<#Entware_list#>"},
+					{url: "NULL", tabName: "__INHERIT__"}
+				]
+			},
+			{
+				menuName: "<#Softcenter#>",
+				index: "menu_Split",
+				tab: [
+					{url: "NULL", tabName: "__HIDE__"}
+				]
+			},
+			{
+				menuName: "<#Softcenter#>",
+				index: "menu_Softcenter",
+				tab: [
+					{url: "Main_Soft_center.asp", tabName: "<#Softcenter#>"},
+					{url: "Main_Soft_setting.asp", tabName: "ManualInstall"},
 					{url: "NULL", tabName: "__INHERIT__"}
 				]
 			}
@@ -460,6 +502,16 @@ define(function(){
 						retArray.push("menu_Alexa_IFTTT");
 					}
 				}
+				if (!softcenter_support || '<% nvram_get("sc_installed"); %>' != '1'){
+					retArray.push("menu_Softcenter");
+				}
+				if (!entware_support){
+					retArray.push("menu_Entware");
+					for(i=0; i<menuTree.list.length; i++){
+						if(menuTree.list[i].menuName == 'Entware')
+							menuTree.list.splice(i,1);
+					}
+				}
 
 				return retArray;
 			},
@@ -468,6 +520,9 @@ define(function(){
 				var retArray = [];
 
 				/* By RC Support */
+				if (!smartdns_support){
+					retArray.push("Advanced_SmartDNS_Content.asp");
+				}
 				if(!bwdpi_support){
 					retArray.push("AdaptiveQoS_Bandwidth_Monitor.asp");
 					retArray.push("AdaptiveQoS_WebHistory.asp");
@@ -768,3 +823,4 @@ define(function(){
 
 	return menuTree;
 });
+
